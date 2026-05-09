000d83f5aca72297d236fb6b87417eeddc5f4e2e
Kjcesar <kaitocesar@gmail.com>
2026-05-09 16:40:46 -0400

    Initial commit

---
+++ quiubo_parce.py
@@ -0,0 +1,12 @@
+import streamlit as streamlit
+
+st.title('Quiubo Parce')
+
+st.write("""
+Esta es mi primera app publicada en streamlit.
+Esta construida en python con streamlit
+         """)
+
+st.info("Pronto esta app va a predecir asteroides peligrosos para el planeta Tierra")
+
+
