94d72819556e706fc592839fbf3a3993a24a4bbd
Kjcesar <kaitocesar@gmail.com>
2026-05-10 17:31:22 -0400

    Ibanasca agregando Parte Final 1

--- ibanasca.py
+++ ibanasca.py
@@ -71,12 +71,8 @@ st.sidebar.markdown(f"**{len(df_filtrado):,} asteroides** con estos filtros")
 
 # parte 3
 #
-tab1, tab2, tab3 = st.tabs(
-    [
-        "📚 Catálogo",
-        "🗺️ Mapas",
-        "🪐 Ficha",
-    ]
+tab1, tab2, tab3, tab4, tab5 = st.tabs(
+    ["📚 Catálogo", "🗺️ Mapas", "🪐 Ficha", "Simulador", "Coincidimos?"]
 )
 
 with tab1:
@@ -97,9 +93,6 @@ with tab1:
         use_container_width=True,
         height=500,
     )
-
-    # tab tab2
-    #
 with tab2:
     st.subheader("Mapa de Peligrosidad")
     st.caption("Los más peligrosos tienen MOID pequeño y H pequeño")
@@ -173,7 +166,6 @@ with tab2:
     fig_familias.update_layout(height=500)
     st.plotly_chart(fig_familias, use_container_width=True)
 
-    # Tab 3
 with tab3:
     st.subheader("Ficha Individual de Asteroide")
 
@@ -224,3 +216,124 @@ with tab3:
                 st.error("Este Asteroide Si es un PHA")
             else:
                 st.success("Este Asteroide No es un PHA")
+
+with tab4:
+    st.subheader("Simulador de Impacto")
+    st.caption("Calcula la energía y el cráter estimado para un impacto asteroidal")
+
+    col_izq, col_der = st.columns(2)
+
+    with col_izq:
+        diametro_km = st.slider(
+            "Diametro del asteroide (km)",
+            min_value=0.01,
+            max_value=20.0,
+            value=0.14,
+            step=0.01,
+            format="%.2f km",
+        )
+
+        tipo = st.selectbox(
+            "Tipo de Asteroide",
+            [
+                "Tipo C - Carbonáceo (1,400 kg/m³)",
+                "Tipo S - Siliceo (2,700 kg/m³)",
+                "Tipo M - Metálico (5,000 kg/m³)",
+            ],
+        )
+
+    with col_der:
+        velocidad_kms = st.slider(
+            "Velocidad de impacto (km/s)",
+            min_value=11.0,
+            max_value=70.0,
+            value=20.0,
+            step=0.5,
+            format="%.1f km/s",
+        )
+
+        angulo = st.slider(
+            "Angulo de impacto (°)",
+            min_value=10,
+            max_value=90,
+            value=45,
+            help="90° = impacto vertical directo. Los impactos oblicuos son más frecuentes.",
+        )
+
+    # ---Calculos Fisicos
+    densidades = {
+        "Tipo C - Carbonáceo (1,400 kg/m³)": 1400,
+        "Tipo S - Siliceo (2,700 kg/m³)": 2700,
+        "Tipo M - Metálico (5,000 kg/m³)": 5000,
+    }
+    rho = densidades[tipo]
+    r_m = (diametro_km * 1000) / 2
+    volumen = (4 / 3) * np.pi * r_m**3
+    masa_kg = rho * volumen
+    v_ms = velocidad_kms * 1000
+    E_J = 0.5 * masa_kg * v_ms**2
+    E_MT = E_J / 4.184e15
+    crater_km = diametro_km * 20
+
+    st.divider()
+
+    # -- Resultado
+    st.subheader("Resultados")
+    (
+        c1,
+        c2,
+        c3,
+        c4,
+    ) = st.columns(4)
+
+    c1.metric("Masa", f"{masa_kg:.2e} kg")
+    c2.metric("Energía cinética", f"{E_J:.2e} J")
+    c3.metric("Energía en Mt TNT", f"{E_MT:.2e} Mt")
+    c4.metric("Cráter estimado", f"{crater_km:.2f} km")
+
+    st.divider()
+
+    # Comparacion Historica
+    st.subheader("Que tan grande es esa energía")
+
+    referencias = {
+        "Bomba de Hiroshima": 0.015,
+        "Chelyabinsk 2013": 0.5,
+        "Bomba Castle Bravo (EEUU)": 15.0,
+        "Krakatoa 1883": 200,
+        "Chicxulub (~KT)": 100_000_000.0,
+    }
+
+    filas = []
+    for evento, e_ref in referencias.items():
+        if E_MT >= e_ref:
+            ratio = E_MT / e_ref
+            comparacion = f"{ratio:.1f} x mas grande"
+        else:
+            ratio = e_ref / E_MT
+            comparacion = f"{ratio:.1f} x mas pequeño"
+
+        filas.append(
+            {
+                "Evento de referencia": evento,
+                "Energia (Mt TNT)": e_ref,
+                "Comparacion": comparacion,
+            }
+        )
+
+    st.dataframe(pd.DataFrame(filas), use_container_width=True, hide_index=True)
+
+    if E_MT < 0.015:
+        st.success("Energia menor a Hiroshima - Evento local menor")
+    elif E_MT < 15:
+        st.warning(
+            "Energia entre Hiroshima y Castle Bravo - Evento Regional Severo ⚠️🌋"
+        )
+    elif E_MT < 10_000:
+        st.error(
+            "Energia entre Castle Bravo y Krakatoa - Evento Continental Catastrofico ⚠️☄️"
+        )
+    else:
+        st.error(
+            "Energia comparable o superior a Chicxulub - Evento de Extincion Masiva 🌍☄️💀"
+        )
